ad7b66de80a10fcd191f2848f7a53f9f50eec8f5
ken-ding <ken.ding@rea-group.com>
2018-03-23 10:59:16 +1100

    Add doubleEveryOther

--- week-1/exercise-01.hs
+++ week-1/exercise-01.hs
@@ -3,11 +3,21 @@ toDigits :: Integer -> [Integer]
 toDigits n =
   if n <= 0
     then []
-    else toDigits (div n 10) ++ mod n 10 : []
+    else toDigits (div n 10) ++ [mod n 10]
 
 toDigitsRev :: Integer -> [Integer]
 toDigitsRev n =
   if n <= 0
     then []
     else mod n 10 : toDigitsRev (div n 10)
+
+doubleEveryOther :: [Integer] -> [Integer]
+doubleEveryOther l =
+  case l of
+    (h : t) ->
+      if (mod (length l) 2 == 0)
+        then h * 2 : doubleEveryOther t
+        else h : doubleEveryOther t
+    [] -> []
+
 
