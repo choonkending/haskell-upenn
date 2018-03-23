178272d1507d0d7506c6ee857756053de8a23662
ken-ding <ken.ding@rea-group.com>
2018-03-23 15:27:38 +1100

    Tower of hanoi

--- week-1/exercise-01.hs
+++ week-1/exercise-01.hs
@@ -28,3 +28,15 @@ sumDigits = foldr ((+) . sum . toDigits) 0
 
 validate :: Integer -> Bool
 validate = (== 0) . (flip mod 10) . sumDigits . doubleEveryOther . toDigits
+
+
+-- Given the number of discs and names for the three pegs,
+-- hanoi should return a list of moves to be performed to
+-- move the stack of discss from the first peg to the second.
+type Peg = String
+type Move = (Peg, Peg)
+hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
+hanoi n a b c
+  | n == 1 = [(a, b)]
+  | otherwise = hanoi (n - 1) a c b ++ [(a, b)] ++ hanoi (n - 1) c b a
+
