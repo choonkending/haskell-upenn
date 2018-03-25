f9462c5678aa2f20f9986b6c7710ac4237da6747
Ken Ding <aenkil@gmail.com>
2018-03-25 20:37:55 +1100

    Initial parseMessage

--- week-2/LogAnalysis.hs
+++ week-2/LogAnalysis.hs
@@ -3,7 +3,16 @@ module LogAnalysis where
 
 import Log
 
-parseMessage :: String -> LogMessage
--- parseMessage l =
---   words l
+convertStringToInt :: String -> Int
+convertStringToInt s = read s :: Int
 
+parseMessage :: String -> LogMessage
+parseMessage s
+  | h == "I" = LogMessage Info (convertStringToInt (head t)) (unwords (tail t))
+  | h == "W" = LogMessage Warning (convertStringToInt (head t)) (unwords (tail t))
+  | h == "E" = LogMessage (Error (convertStringToInt (head t))) (convertStringToInt (head (drop 1 t))) (unwords (drop 2 t))
+  | otherwise = Unknown s
+  where
+    l = words s
+    h = head l
+    t = tail l
