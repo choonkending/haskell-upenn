72115a3f5a3617f7448123052c3d7af8e95b2ff6
Ken Ding <aenkil@gmail.com>
2018-04-14 11:34:01 +1000

    Clean up pattern matching

--- week-2/LogAnalysis.hs
+++ week-2/LogAnalysis.hs
@@ -37,12 +37,16 @@ parse :: String -> [LogMessage]
 parse = map parseMessage . lines
 
 insert :: LogMessage -> MessageTree -> MessageTree
-insert (Unknown _) = id
-insert l@(LogMessage _ t1 _) Leaf = Node Leaf l Leaf
-insert l1@(LogMessage _ t1 _)
-  (Node leftTree l2@(LogMessage _ t2 _) rightTree) =
-    if t1 < t2
-      then Node (insert l1 leftTree) l2 rightTree
-      else Node leftTree l2 (insert l1 rightTree)
+insert (Unknown _) m = m
+insert l@(LogMessage _ _ _) Leaf = Node Leaf l Leaf
+-- insert l1@(LogMessage _ t1 _)
+--   (Node leftTree l2@(LogMessage _ t2 _) rightTree) =
+--     if t1 < t2
+--       then Node (insert l1 leftTree) l2 rightTree
+--       else Node leftTree l2 (insert l1 rightTree)
 
+insert l1@(LogMessage _ t1 _)
+  (Node leftTree l2@(LogMessage _ t2 _) rightTree)
+  | t1 < t2 = Node (insert l2 leftTree) l2 rightTree
+  | otherwise = Node leftTree l2 (insert l1 rightTree)
 
